15cd78a2ff39a2b0c31d02520429212c69fbc958
Olle Kruber <ollekruber@gmail.com>
2025-03-10 14:03:58 +0100

    kamikazeeeee

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,5 +1,6 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+import logging
 
 class DataflowOptions(PipelineOptions):
     @classmethod
@@ -8,14 +9,27 @@ class DataflowOptions(PipelineOptions):
         parser.add_value_provider_argument('--output', type=str, help='Output file path')
 
 def run(argv=None):
+    logging.getLogger().setLevel(logging.INFO)
+    
     pipeline_options = PipelineOptions()
     dataflow_options = pipeline_options.view_as(DataflowOptions)
     
-    with beam.Pipeline(options=pipeline_options) as p:
-        (p
-         | 'Read' >> beam.io.ReadFromText(dataflow_options.input)
-         | 'Transform' >> beam.Map(lambda x: x.upper())
-         | 'Write' >> beam.io.WriteToText(dataflow_options.output))
+    try:
+        logging.info('Pipeline starting...')
+        print('Pipeline running...')
+
+        with beam.Pipeline(options=pipeline_options) as p:
+            (p
+             | 'Read' >> beam.io.ReadFromText(dataflow_options.input)
+             | 'Transform' >> beam.Map(lambda x: x.upper())
+             | 'Write' >> beam.io.WriteToText(dataflow_options.output))
+            
+        logging.info('Pipeline completed successfully!')
+        print('Pipeline execution completed successfully!')
+        
+    except Exception as e:
+        logging.error(f'Pipeline failed with error: {str(e)}')
+        raise
 
 if __name__ == '__main__':
     run()
